4716005feb742cc0e468097a40259e8aec0d7a69
HeavyJS <whereorange@gmail.com>
2019-01-16 20:23:02 +1000

    content

---
+++ moit_lab/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moit_lab.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public long Id { get; set; }//PK syntax
+
+        [StringLength(30, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z0-9_.-]*$")]
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [StringLength(40, MinimumLength = 5)]
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(60, MinimumLength = 5)]
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [StringLength(60, MinimumLength = 5)]
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string PasswordConformarion { get; set; }
+
+        public string Salt { get; set; }
+        public string Role { get; set; }
+
+        //    [Required]
+        //    [Display(Name = "Email")]
+        //    public string Email { get; set; }
+
+        //    [Required]
+        //    [DataType(DataType.Password)]
+        //    [Display(Name = "Пароль")]
+        //    public string Password { get; set; }
+
+        //    [Required]
+        //    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        //    [DataType(DataType.Password)]
+        //    [Display(Name = "Подтвердить пароль")]
+        //    public string PasswordConfirm { get; set; }
+        //}
+    }
+}
